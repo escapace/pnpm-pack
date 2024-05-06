99aae0644ad0aa641d4a4a29c5ec5f3c38a1af6c
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-06 05:45:46 +0000

    fix: updates

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import arg from 'arg'
+import assert from 'node:assert'
+import { assertRequirements } from './utilities/assert-requirements'
+import { exit } from './utilities/exit'
+
+await assertRequirements()
+
+const { _ } = arg({}, { permissive: true })
+
+assert((_.length = 1))
+const command = _[0]
+assert(command === 'workspace' || command === 'package' || command === 'cleanup')
+
+// prettier-ignore
+const run = await ({
+  cleanup: async () => (await import('./pack-cleanup')).packCleanup,
+  package: async () => (await import('./pack-package')).packPackage,
+  workspace: async () => (await import('./pack-workspace')).packWorkspace
+}[command]())
+
+const error = await run()
+exit(error)

6f92f1d88caaf267af21b098f166cdbdaf196209
Mark Martirosian <hello@markmartirosian.com>
2024-05-08 09:24:21 +0000

    feat: build and optionally extract

--- src/pack-package.ts
+++ src/pack-package.ts
@@ -1,13 +1,14 @@
 import arg from 'arg'
 import { execa } from 'execa'
 import fse from 'fs-extra'
+import { kebabCase } from 'lodash-es'
 import assert from 'node:assert'
 import { mkdtemp, writeFile } from 'node:fs/promises'
 import os from 'node:os'
 import path from 'node:path'
 import process from 'node:process'
 import { isNativeError } from 'node:util/types'
-import semver from 'semver'
+import { argumentsCommon, argumentsCommonParse } from './arguments-common'
 import { getNameArchive } from './utilities/get-name-archive'
 import { getPathDirectoryPackage } from './utilities/get-path-directory-package'
 import { getPathDirectoryWorkspace } from './utilities/get-path-directory-workspace'
@@ -17,17 +18,21 @@ export async function packPackage() {
   let error: Error | undefined
   let pathDirectoryTemporary: string | undefined
 
-  const options = arg({
-    '--no-build': Boolean,
+  const arguments_ = arg({
+    '--extract': Boolean,
     '--no-cleanup': Boolean,
-    '--pack-destination': String,
     '--skip-workspace-root': Boolean,
     '--temporary-directory': String,
-    '--version': String,
+    ...argumentsCommon,
   })
 
-  const flagCleanup = options['--no-cleanup'] !== true
-  const flagBuild = options['--no-build'] !== true
+  const options = {
+    ...argumentsCommonParse(arguments_),
+    cleanup: arguments_['--no-cleanup'] !== true,
+    extract: arguments_['--extract'] === true,
+    skipWorkspaceRoot: arguments_['--skip-workspace-root'] === true,
+    temporaryDirectory: arguments_['--temporary-directory'],
+  }
 
   const pathDirectoryPackage = await getPathDirectoryPackage(process.cwd())
   const pathDirectoryRoot =
@@ -35,10 +40,7 @@ export async function packPackage() {
 
   process.chdir(pathDirectoryPackage)
 
-  const pathDirectoryDestination = path.resolve(
-    pathDirectoryPackage,
-    options['--pack-destination'] ?? 'lib',
-  )
+  const pathDirectoryDestination = path.resolve(pathDirectoryPackage, options.packDestination)
 
   const pathDirectoryPackageRelative = path.relative(pathDirectoryRoot, pathDirectoryPackage)
 
@@ -46,39 +48,39 @@ export async function packPackage() {
   const pathPackageJSON = path.join(pathDirectoryPackage, 'package.json')
   const packageJSON = await readPackageJSON(pathDirectoryPackage)
 
-  const version = options['--version'] ?? '0.0.0'
-
-  assert(typeof semver.valid(version) === 'string')
-
-  await writeFile(pathPackageJSON, JSON.stringify(Object.assign(packageJSON, { version }), null, 2))
+  await writeFile(
+    pathPackageJSON,
+    JSON.stringify(Object.assign(packageJSON, { version: options.version }), null, 2),
+  )
 
   try {
-    if (typeof packageJSON.scripts?.build === 'string' && flagBuild) {
+    if (typeof packageJSON.scripts?.build === 'string' && options.build) {
       await execa('pnpm', ['run', 'build'], {
         stdio: 'inherit',
       })
     }
 
     pathDirectoryTemporary =
-      typeof options['--temporary-directory'] === 'string'
-        ? path.resolve(pathDirectoryPackage, options['--temporary-directory'])
+      typeof options.temporaryDirectory === 'string'
+        ? path.resolve(pathDirectoryPackage, options.temporaryDirectory)
         : await mkdtemp(path.join(os.tmpdir(), 'pnpm-pack'))
 
     const pathDirectoryContext = path.join(pathDirectoryTemporary, 'package')
 
     // https://github.com/pnpm/pnpm/blob/6caec8109b563fd27ad262ce06a1f587cebbc044/releasing/plugin-commands-publishing/src/pack.ts#L98C1-L98C100
-    const nameArchive = getNameArchive({ name: packageJSON.name, version })
+    const nameArchive = getNameArchive({ name: packageJSON.name, version: options.version })
     const pathFileArchive = path.join(pathDirectoryTemporary, nameArchive)
 
     await execa('pnpm', ['pack', '--pack-destination', pathDirectoryTemporary], {
       stdio: 'inherit',
     })
-    assert(fse.exists(pathFileArchive))
+    assert(await fse.exists(pathFileArchive), `${pathFileArchive}: No such file`)
     const pathDirectoryPackageContext = path.join(
       pathDirectoryContext,
       pathDirectoryPackageRelative,
     )
     await fse.mkdirp(pathDirectoryPackageContext)
+
     await execa(
       'tar',
       ['-xf', pathFileArchive, '--strip-components=1', '-C', pathDirectoryPackageContext],
@@ -87,19 +89,72 @@ export async function packPackage() {
       },
     )
 
-    if (isRoot && options['--skip-workspace-root'] === true) {
+    if (options.deployment) {
+      const pathDirectoryDeployment = path.join(
+        pathDirectoryTemporary,
+        kebabCase(`${nameArchive}-deployment`),
+      )
+
+      await execa(
+        'pnpm',
+        [
+          'deploy',
+          ...[
+            options.development ? '--dev' : undefined,
+            options.optional ? undefined : '--no-optional',
+            options.production ? '--prod' : undefined,
+          ].filter((value): value is string => typeof value === 'string'),
+          '--filter',
+          '.',
+          pathDirectoryDeployment,
+        ],
+        {
+          stdio: 'inherit',
+        },
+      )
+
+      const pathNodeModules = path.join(pathDirectoryDeployment, 'node_modules')
+
+      if (await fse.exists(pathNodeModules)) {
+        await fse.move(pathNodeModules, path.join(pathDirectoryPackageContext, 'node_modules'))
+      }
+
+      await execa(
+        'tar',
+        ['-czf', pathFileArchive, '--strip-components=1', '-C', pathDirectoryTemporary, 'package'],
+        {
+          stdio: 'inherit',
+        },
+      )
+    }
+
+    if (isRoot && options.skipWorkspaceRoot) {
       await fse.remove(pathFileArchive)
     } else {
       await fse.mkdirp(pathDirectoryPackageContext)
-      await fse.move(pathFileArchive, path.join(pathDirectoryDestination, nameArchive), {
-        overwrite: true,
-      })
+      if (options.extract) {
+        await fse.emptydir(pathDirectoryDestination)
+
+        await execa(
+          'tar',
+          ['-xf', pathFileArchive, '--strip-components=1', '-C', pathDirectoryDestination],
+          {
+            stdio: 'inherit',
+          },
+        )
+      } else {
+        await fse.move(pathFileArchive, path.join(pathDirectoryDestination, nameArchive), {
+          overwrite: true,
+        })
+      }
     }
   } catch (error_) {
     error = isNativeError(error_) ? error_ : new Error('Unknown Error')
+
+    console.log(error)
   }
 
-  if (flagCleanup) {
+  if (options.cleanup) {
     await writeFile(
       pathPackageJSON,
       JSON.stringify(Object.assign(packageJSON, { version: '0.0.0' }), null, 2),
--- src/pack-workspace.ts
+++ src/pack-workspace.ts
@@ -1,21 +1,17 @@
+import { readWantedLockfileAndAutofixConflicts, writeWantedLockfile } from '@pnpm/lockfile-file'
 import arg from 'arg'
 import { execa } from 'execa'
 import fse from 'fs-extra'
+import { mapValues } from 'lodash-es'
 import assert from 'node:assert'
 import { mkdtemp } from 'node:fs/promises'
-import { isNativeError } from 'node:util/types'
 import os from 'node:os'
 import path from 'node:path'
-import semver from 'semver'
-import { fileURLToPath } from 'node:url'
+import { isNativeError } from 'node:util/types'
+import { argumentsCommon, argumentsCommonParse } from './arguments-common'
 import { getNameArchive } from './utilities/get-name-archive'
 import { getPathDirectoryWorkspace } from './utilities/get-path-directory-workspace'
 import { readPackageJSON } from './utilities/read-package-json'
-import {
-  readWantedLockfileAndAutofixConflicts,
-  writeWantedLockfile
-} from '@pnpm/lockfile-file'
-import { mapValues } from 'lodash-es'
 
 export async function packWorkspace() {
   let error: Error | undefined
@@ -28,55 +24,52 @@ export async function packWorkspace() {
   process.chdir(cwd)
 
   // https://pnpm.io/filtering
-  const options = arg(
+  const arguments_ = arg(
     {
       '--changed-files-ignore-pattern': String,
       '--filter': [String],
       '--filter-prod': [String],
-      '--pack-destination': String,
       '--test-pattern': String,
-      '--version': String,
-      '--workspace-concurrency': Number
+      '--workspace-concurrency': Number,
+      ...argumentsCommon,
     },
-    { permissive: false }
+    { permissive: false },
   )
 
-  const version = options['--version'] ?? '0.0.0'
-  assert(typeof semver.valid(version) === 'string')
+  const options = {
+    ...argumentsCommonParse(arguments_),
+  }
 
-  const pathDirectoryDestination = path.resolve(
-    cwd,
-    options['--pack-destination'] ?? 'lib'
-  )
+  const pathDirectoryDestination = path.resolve(cwd, options.packDestination)
 
   const packageJSON = await readPackageJSON(cwd)
-  const nameArchive = getNameArchive({ name: packageJSON.name, version })
+  const nameArchive = getNameArchive({ name: packageJSON.name, version: options.version })
 
   const filters = [
-    ...(Array.isArray(options['--filter-prod'])
-      ? options['--filter-prod'].flatMap((value) => ['--filter-prod', value])
+    ...(Array.isArray(arguments_['--filter-prod'])
+      ? arguments_['--filter-prod'].flatMap((value) => ['--filter-prod', value])
       : []),
-    ...(Array.isArray(options['--filter'])
-      ? options['--filter'].flatMap((value) => ['--filter', value])
-      : [])
+    ...(Array.isArray(arguments_['--filter'])
+      ? arguments_['--filter'].flatMap((value) => ['--filter', value])
+      : []),
   ].filter((value): value is string => typeof value === 'string')
 
   const pnpmExecArguments = [
     '--fail-if-no-match',
     '--workspace-root',
-    typeof options['--workspace-concurrency'] === 'number'
-      ? `--workspace-concurrency=${options['--workspace-concurrency']}`
+    typeof arguments_['--workspace-concurrency'] === 'number'
+      ? `--workspace-concurrency=${arguments_['--workspace-concurrency']}`
       : undefined,
-    typeof options['--test-pattern'] === 'string'
-      ? `--test-pattern=${options['--test-pattern']}`
+    typeof arguments_['--test-pattern'] === 'string'
+      ? `--test-pattern=${arguments_['--test-pattern']}`
       : undefined,
-    typeof options['--changed-files-ignore-pattern'] === 'string'
-      ? `--changed-files-ignore-pattern=${options['--changed-files-ignore-pattern']}`
+    typeof arguments_['--changed-files-ignore-pattern'] === 'string'
+      ? `--changed-files-ignore-pattern=${arguments_['--changed-files-ignore-pattern']}`
       : undefined,
     ...(filters.length === 0 ? ['--filter', '*'] : filters),
     'exec',
     'node',
-    fileURLToPath(import.meta.url)
+    path.join(import.meta.dirname, 'cli.js'),
   ]
 
   try {
@@ -89,34 +82,35 @@ export async function packWorkspace() {
       [
         ...pnpmExecArguments,
         'package',
+        options.development ? '--development' : undefined,
+        options.optional ? undefined : '--no-optional',
+        options.production ? '--production' : undefined,
         '--skip-workspace-root',
         '--no-cleanup',
         '--temporary-directory',
         pathDirectoryTemporary,
         '--version',
-        version
+        options.version,
       ].filter((value): value is string => typeof value === 'string'),
       {
         cwd,
-        stdio: 'inherit'
-      }
+        stdio: 'inherit',
+      },
     )
 
     const directoryPathContext = path.join(pathDirectoryTemporary, 'package')
     const pathFileArchive = path.join(pathDirectoryTemporary, nameArchive)
 
-    assert(fse.exists(directoryPathContext))
-    assert(fse.exists(pathFileArchive))
+    assert(await fse.exists(directoryPathContext), `${directoryPathContext}: No such directory`)
 
     await execa('pnpm', ['install', '--lockfile-only', '--ignore-scripts'], {
       cwd,
-      stdio: 'inherit'
+      stdio: 'inherit',
     })
 
-    const { lockfile } = await readWantedLockfileAndAutofixConflicts(
-      directoryPathContext,
-      { ignoreIncompatible: false }
-    )
+    const { lockfile } = await readWantedLockfileAndAutofixConflicts(directoryPathContext, {
+      ignoreIncompatible: false,
+    })
 
     assert(lockfile !== null)
 
@@ -124,51 +118,45 @@ export async function packWorkspace() {
       ...value,
       specifiers: mapValues(value.specifiers, (value) => {
         if (value.startsWith('workspace:')) {
-          return version
+          return options.version
         }
 
         return value
-      })
+      }),
     }))
 
     lockfile.importers = importers
 
     await writeWantedLockfile(directoryPathContext, lockfile)
 
-    await execa(
-      'tar',
-      ['-czf', pathFileArchive, '-C', pathDirectoryTemporary, 'package'],
-      {
-        cwd,
-        stdio: 'inherit'
-      }
-    )
+    await execa('tar', ['-czf', pathFileArchive, '-C', pathDirectoryTemporary, 'package'], {
+      cwd,
+      stdio: 'inherit',
+    })
+
+    assert(await fse.exists(pathFileArchive), `${pathFileArchive}: No such file`)
 
     await fse.remove(directoryPathContext)
     await fse.mkdirp(pathDirectoryDestination)
-    await fse.move(
-      pathFileArchive,
-      path.join(pathDirectoryDestination, nameArchive),
-      { overwrite: true }
-    )
+    await fse.move(pathFileArchive, path.join(pathDirectoryDestination, nameArchive), {
+      overwrite: true,
+    })
   } catch (error_) {
     error = isNativeError(error_) ? error_ : new Error('Unknown Error')
   }
 
   await execa(
     'pnpm',
-    [...pnpmExecArguments, 'cleanup'].filter(
-      (value): value is string => typeof value === 'string'
-    ),
+    [...pnpmExecArguments, 'cleanup'].filter((value): value is string => typeof value === 'string'),
     {
       cwd,
-      stdio: 'inherit'
-    }
+      stdio: 'inherit',
+    },
   )
 
   await execa('pnpm', ['install', '--lockfile-only', '--ignore-scripts'], {
     cwd,
-    stdio: 'inherit'
+    stdio: 'inherit',
   })
 
   if (typeof pathDirectoryTemporary === 'string') {

042054bbf76712c06677c9b00d2084c5402c6a11
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-08 09:24:21 +0000

    feat: build and optionally extract

---
+++ src/arguments-common.ts
@@ -0,0 +1,36 @@
+import type arg from 'arg'
+import assert from 'node:assert'
+import semver from 'semver'
+
+export const argumentsCommon = {
+  '--development': Boolean,
+  '--no-build': Boolean,
+  '--no-optional': Boolean,
+  '--pack-destination': String,
+  '--production': Boolean,
+  '--version': String,
+} as const
+
+export const argumentsCommonParse = <T extends arg.Result<typeof argumentsCommon>>(options: T) => {
+  const version = options['--version'] ?? '0.0.0'
+  assert(typeof semver.valid(version) === 'string')
+  const build = options['--no-build'] !== true
+
+  const development = options['--development'] === true
+  const optional = options['--no-optional'] !== true
+  const production = options['--production'] === true
+
+  const deployment = development || production
+
+  const packDestination = options['--pack-destination'] ?? 'lib'
+
+  return {
+    build,
+    deployment,
+    development,
+    optional,
+    packDestination,
+    production,
+    version,
+  }
+}

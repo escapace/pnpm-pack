0d2b332a51fcea92a3bc7673ce51e0640ef1c7e8
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-23 23:01:39 +0000

    fix: lockfile

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import { version } from './package.json'
 
 export default defineConfig({
+  define: {
+    __PLATFORM__: JSON.stringify('node'),
+    __VERSION__: JSON.stringify(version),
+  },
   test: {
     coverage: {
       exclude: ['**/example*.ts'],
